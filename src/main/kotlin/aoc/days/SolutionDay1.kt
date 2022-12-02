7d3a27d7081369b9b5c6de44304eb7d3815b7544
michal-wisniewski <michal.w.wisniewski@gmail.com>
2022-12-02 18:32:34 +0100

    day1 solution v2

--- src/main/kotlin/aoc/days/SolutionDay1.kt
+++ src/main/kotlin/aoc/days/SolutionDay1.kt
@@ -5,53 +5,24 @@ import aoc.utils.Reader
 class SolutionDay1 {
 
     fun partOne() : Any {
-        var maxCalories = 0
-        var currentCalories = 0
-
-        Reader.inputAsStringList(1)
-            .map { it.toIntOrNull() }
-            .forEach {
-                if (it == null) {
-                    maxCalories = maxOf(maxCalories, currentCalories)
-                    currentCalories = 0
-                    return@forEach
-                }
-                currentCalories += it
-            }
-
-        return maxCalories
+        return parseInput(Reader.inputAsString(1))
+            .map { it.sum() }
+            .sortedDescending()
+            .take(1)
+            .sum()
     }
 
     fun partTwo() : Any {
-        var firstLargestCalories = -1
-        var secondLargestCalories = -2
-        var thirdLargestCalories = -3
+        return parseInput(Reader.inputAsString(1))
+            .map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
+    }
 
-        var currentCalories = 0
-
-        Reader.inputAsStringList(1)
-            .map { it.toIntOrNull() }
-            .forEach {
-                if (it == null) {
-
-                    if (currentCalories > firstLargestCalories) {
-                        thirdLargestCalories = secondLargestCalories
-                        secondLargestCalories = firstLargestCalories
-                        firstLargestCalories = currentCalories
-                    }
-                    else if (currentCalories >= secondLargestCalories && currentCalories < firstLargestCalories) {
-                        thirdLargestCalories = secondLargestCalories
-                        secondLargestCalories = currentCalories
-                    }
-                    else if (currentCalories >= thirdLargestCalories && currentCalories < secondLargestCalories)
-                        thirdLargestCalories = currentCalories
-
-                    currentCalories = 0
-                    return@forEach
-                }
-                currentCalories += it
-            }
-
-        return firstLargestCalories + secondLargestCalories + thirdLargestCalories
+    private fun parseInput(input: String): List<List<Int>> {
+       return input.split("\n\n")
+            .map { it.lines().map { it.toInt() } }
+            .toList()
     }
 }

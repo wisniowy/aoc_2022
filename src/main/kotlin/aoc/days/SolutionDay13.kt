95dd89e92b6d188fcf71813867f27e79489ee0b2
michal-wisniewski <michal.w.wisniewski@gmail.com>
2022-12-14 16:17:31 +0100

    day 13  refactor

--- src/main/kotlin/aoc/days/SolutionDay13.kt
+++ src/main/kotlin/aoc/days/SolutionDay13.kt
@@ -4,53 +4,23 @@ import aoc.utils.Reader
 
 class SolutionDay13 : SolutionDay(13) {
 
-    private fun compareNestedValues(left: NestedValue, right: NestedValue): Int {
-        if (left.value != null && right.value != null) return compareNestedValues(left.value, right.value)
-        else if (left.value == null && right.value == null) return compareNestedValues(left.nestedValues, right.nestedValues)
-        else if (left.value != null) return compareNestedValues(NestedValue(null, listOf(left)), right)
-        else if (right.value != null) return compareNestedValues(left, NestedValue(null, listOf(right)))
-        return 3
+    private enum class ComparisonResult {
+        RIGHT_ORDER, WRONG_ORDER, CONTINUE
     }
 
-    private fun compareNestedValues(left: Int, right: Int): Int {
-        return when {
-            left < right -> 0
-            left > right -> 1
-            else -> 3
-        }
-    }
+    data class NestedValue(val value: Int?, val nestedValues: List<NestedValue>)
+    data class ParserOutput(val nestedValue: NestedValue, val lineIdx: Int)
+    data class NestedValueWrapper(val nestedValue: NestedValue, val divider: Boolean)
 
-    private fun compareNestedValues(left: List<NestedValue>, right: List<NestedValue>): Int {
-        var idx = 0
-
-        while (idx < left.size && idx < right.size) {
-            when (compareNestedValues(left[idx], right[idx])) {
-                0 -> return 0
-                1 -> return 1
-            }
-
-            idx += 1
-        }
-
-        return when {
-            (idx == (left.size)) && (idx == (right.size)) -> 3
-            idx == right.size -> 1
-            idx == left.size -> 0
-            else -> 3
-        }
-    }
 
     override fun partOne(): Any {
         val pairsOfPackets = parseInput(Reader.inputAsString(day_n))
 
         return pairsOfPackets.mapIndexed { idx, it -> Pair(idx + 1, compareNestedValues(it.first, it.second)) }
-            .filter { it.second == 0 }
+            .filter { it.second == ComparisonResult.RIGHT_ORDER }
             .map { it.first }
             .sum()
     }
-
-
-    data class NestedValueWrapper(val nestedValue: NestedValue, val divider: Boolean)
 
     override fun partTwo(): Any {
         val packets = parseInput(Reader.inputAsString(day_n)).map { it -> listOf(it.first, it.second) }
@@ -61,11 +31,10 @@ class SolutionDay13 : SolutionDay(13) {
         packets.add(NestedValueWrapper(parseList("[[2]]", 1).nestedValue, true))
         packets.add(NestedValueWrapper(parseList("[[6]]", 1).nestedValue, true))
 
-        val myCustomComparator =  Comparator<NestedValueWrapper> { a, b ->
-            val comparisonResult = compareNestedValues(a.nestedValue, b.nestedValue)
-            when (comparisonResult) {
-                0 -> -1
-                1 -> 1
+        val myCustomComparator = Comparator<NestedValueWrapper> { a, b ->
+            when (compareNestedValues(a.nestedValue, b.nestedValue)) {
+                ComparisonResult.RIGHT_ORDER -> -1
+                ComparisonResult.WRONG_ORDER -> 1
                 else -> 0
             }
         }
@@ -77,7 +46,43 @@ class SolutionDay13 : SolutionDay(13) {
                 true -> idx + 1
                 false -> 1
             }
-        }.reduce{ a: Int, b: Int -> a * b }
+        }.reduce { a: Int, b: Int -> a * b }
+    }
+
+    private fun compareNestedValues(left: NestedValue, right: NestedValue): ComparisonResult {
+        if (left.value != null && right.value != null) return compareNestedValues(left.value, right.value)
+        else if (left.value == null && right.value == null) return compareNestedValues(left.nestedValues, right.nestedValues)
+        else if (left.value != null) return compareNestedValues(NestedValue(null, listOf(left)), right)
+        else if (right.value != null) return compareNestedValues(left, NestedValue(null, listOf(right)))
+        return ComparisonResult.CONTINUE
+    }
+
+    private fun compareNestedValues(left: Int, right: Int): ComparisonResult {
+        return when {
+            left < right -> ComparisonResult.RIGHT_ORDER
+            left > right -> ComparisonResult.WRONG_ORDER
+            else -> ComparisonResult.CONTINUE
+        }
+    }
+
+    private fun compareNestedValues(left: List<NestedValue>, right: List<NestedValue>): ComparisonResult {
+        var idx = 0
+
+        while (idx < left.size && idx < right.size) {
+            when (compareNestedValues(left[idx], right[idx])) {
+                ComparisonResult.RIGHT_ORDER -> return ComparisonResult.RIGHT_ORDER
+                ComparisonResult.WRONG_ORDER -> return ComparisonResult.WRONG_ORDER
+                else -> {}
+            }
+            idx += 1
+        }
+
+        return when {
+            (idx == (left.size)) && (idx == (right.size)) -> ComparisonResult.CONTINUE
+            idx == right.size -> return ComparisonResult.WRONG_ORDER
+            idx == left.size -> return ComparisonResult.RIGHT_ORDER
+            else -> return ComparisonResult.CONTINUE
+        }
     }
 
     private fun parseInput(input: String): List<Pair<NestedValue, NestedValue>> {
@@ -86,9 +91,6 @@ class SolutionDay13 : SolutionDay(13) {
             return@map Pair(parseList(splitted[0], 1).nestedValue, parseList(splitted[1], 1).nestedValue)
         }.toList()
     }
-
-    data class NestedValue(val value: Int?, val nestedValues: List<NestedValue>)
-    data class ParserOutput(val nestedValue: NestedValue, val lineIdx: Int)
 
     private fun parseList(line: String, lineIdx: Int): ParserOutput {
         var i = lineIdx
@@ -124,7 +126,7 @@ class SolutionDay13 : SolutionDay(13) {
         while (i < line.length) {
             val c = line[i]
 
-            if(c.isDigit()) {
+            if (c.isDigit()) {
                 digits.add(c)
             } else {
                 return ParserOutput(NestedValue(digits.joinToString("").toInt(), listOf()), i)
